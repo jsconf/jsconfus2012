795fed3fe963ed8ca4aad3b3f3a189bcb90db8f1
Chris Williams <chris@iterativedesigns.com>
2012-01-10 19:58:54 -0500

    Day is done. Gone the Sun.

--- js/jsconf.js
+++ js/jsconf.js
@@ -1,8 +1,25 @@
-
-function initPage() {
-  $('#parallax').jparallax();
+function show(link) {
+  return (function () {
+    $(".pane").hide();    
+    $("#"+link).show();
+    $(".selected").removeClass("selected");
+    $("#nav-"+link).addClass("selected");
+  });
 }
+
+var routes = {
+  '/': show("home"),
+  '/schedule': show("schedule"),
+  '/venue': show("venue"),
+  '/speakers': show("speakers"),
+  '/sponsors': show("sponsors"),
+  '/about': show("about")
+};
+
+var router = Router(routes);
+router.init();
+
+
+
 $(function () {
-  $(window).resize(initPage)
-  initPage();
 });
